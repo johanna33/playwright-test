bbadefe5b45479b64c17d74ea8369ae117d28a24
Johanna Calderon <joha_17@hotmail.com>
2025-03-22 19:23:04 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,8 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: 0,
+  workers: process.env.CI ? 4 : undefined,
   reporter: 'html',
 
   use: {
