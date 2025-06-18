1094fbb477954687606f73987dda52b51db40cee
Johanna Calderon <joha_17@hotmail.com>
2025-06-17 20:49:03 -0500

    fix api failing test

--- tests/api/specs/api-account.spec.ts
+++ tests/api/specs/api-account.spec.ts
@@ -1,44 +1,52 @@
 import { test, expect } from "../../setup/base-fixture";
-import { getAccountDetails, getAccounts, updateCustomer } from '../request/account';
+import {
+  getAccountDetails,
+  getAccounts,
+  updateCustomer,
+} from "../request/account";
 
 test.describe.configure({ mode: "serial" });
 
 test.describe("Account API", () => {
-    let customerId: string;
-    let accountId: string;
-  
-    test.beforeEach("get the customerId in the update contact info page", async ({ pm }) => {
+  let customerId: string;
+  let accountId: string;
+
+  test.beforeEach(
+    "get the customerId in the update contact info page",
+    async ({ pm }) => {
+      if (!customerId) {
         await pm.getAccountsPage().openAccountOverviewPage();
         await pm.getAccountsPage().openUpdateContactInfoPage();
         customerId = await pm.getAccountsPage().getCustomerId();
-    });
+      }
+    }
+  );
 
-    test('Verify account overview request', async () => {
-        const response = await getAccounts(customerId);
-        expect(response.status()).toBe(200);
-        const responseBody = await response.json();
-        expect(responseBody[0]).toHaveProperty('id');
-        expect(responseBody[0]).toHaveProperty('customerId');
-        expect(responseBody[0]).toHaveProperty('type', 'CHECKING');
-        expect(responseBody[0]).toHaveProperty('balance');
-        accountId = responseBody[0].id;
-    });
+  test("Verify account overview request", async () => {
+    const response = await getAccounts(customerId);
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody[0]).toHaveProperty("id");
+    expect(responseBody[0]).toHaveProperty("customerId");
+    expect(responseBody[0]).toHaveProperty("type", "CHECKING");
+    expect(responseBody[0]).toHaveProperty("balance");
+    accountId = responseBody[0].id;
+  });
 
-    test('Verify account details request', async () => {
-        const response = await getAccountDetails(accountId);
-        expect(response.status()).toBe(200);
-        const responseBody = await response.json();
-        expect(responseBody).toHaveProperty('id', accountId);
-        expect(responseBody).toHaveProperty('customerId');
-        expect(responseBody).toHaveProperty('type', 'CHECKING');
-        expect(responseBody).toHaveProperty('balance');
-    });
+  test("Verify account details request", async () => {
+    const response = await getAccountDetails(accountId);
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody).toHaveProperty("id", accountId);
+    expect(responseBody).toHaveProperty("customerId");
+    expect(responseBody).toHaveProperty("type", "CHECKING");
+    expect(responseBody).toHaveProperty("balance");
+  });
 
-    test('Verify update customer request', async () => {
-        const response = await updateCustomer(customerId);
-        expect(response.status()).toBe(200);
-        const responseBody = await response.json();
-        expect(responseBody).toEqual("Successfully updated customer profile");
-    });
-
+  test("Verify update customer request", async () => {
+    const response = await updateCustomer(customerId);
+    expect(response.status()).toBe(200);
+    const responseBody = await response.text();
+    expect(responseBody).toEqual("Successfully updated customer profile");
+  });
 });
--- tests/common/test-data/user-data.ts
+++ tests/common/test-data/user-data.ts
@@ -1,23 +1,25 @@
 import { UserAccountData } from "../interfaces/user-interface";
 import { faker } from "@faker-js/faker";
 
-/*Note: The credentials were added here for this demo, in a real scenario, they will be stored in a .env file
-*       or a vault in case of a CI/CD pipeline, for security reasons.
-*/
+/*
+ * Note: The credentials were added here on this demo, in a real scenario,
+ * they will be stored in a .env file or a vault in case of a CI/CD pipeline,
+ * for security reasons.
+ */
 export const credentials = {
-    username: 'mary22',
-    password: 'test1234'
+  username: "mary22",
+  password: "test1234",
 };
 
 export const newUser: UserAccountData = {
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
-    street: faker.location.streetAddress(),
-    city: faker.location.city(),
-    state: faker.location.state(),
-    zipCode: faker.location.zipCode(),
-    phoneNumber: faker.string.numeric(10),
-    ssn: faker.string.numeric(9),
-    username: faker.string.alphanumeric(6),
-    password: faker.internet.password( { length: 6 })
-}
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  street: faker.location.streetAddress(),
+  city: faker.location.city(),
+  state: faker.location.state(),
+  zipCode: faker.location.zipCode(),
+  phoneNumber: faker.string.numeric(10),
+  ssn: faker.string.numeric(9),
+  username: faker.string.alphanumeric(6),
+  password: faker.internet.password({ length: 6 }),
+};
